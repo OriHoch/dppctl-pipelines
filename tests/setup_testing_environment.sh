2e50d2f9089b3e48a778458588d84bde273a2984
Ori Hoch <ori@uumpa.com>
2018-03-31 18:52:53 +0300

    travis

--- tests/setup_testing_environment.sh
+++ tests/setup_testing_environment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get update && sudo apt-get install -y util-linux &&\
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.4/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/ &&\
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ &&\
 curl -L https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sudo bash &&\
