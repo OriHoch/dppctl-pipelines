3c37210d4c7f18af958d924fb9865cf92fef86c3
Ori Hoch <ori@uumpa.com>
2018-03-31 19:02:15 +0300

    travis

--- tests/setup_testing_environment.sh
+++ tests/setup_testing_environment.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get install -y util-linux &&\
+check_or_build_nsenter() {
+    which nsenter >/dev/null && return 0
+    echo "INFO: Building 'nsenter' ..."
+cat <<-EOF | docker run -i --rm -v "$(pwd):/build" ubuntu:14.04 >& nsenter.build.log
+        apt-get update
+        apt-get install -qy git bison build-essential autopoint libtool automake autoconf gettext pkg-config
+        git clone --depth 1 git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git /tmp/util-linux
+        cd /tmp/util-linux
+        ./autogen.sh
+        ./configure --without-python --disable-all-programs --enable-nsenter
+        make nsenter
+        cp -pfv nsenter /build
+EOF
+    if [ ! -f ./nsenter ]; then
+        echo "ERROR: nsenter build failed, log:"
+        cat nsenter.build.log
+        return 1
+    fi
+    echo "INFO: nsenter build OK"
+}
+
+check_or_build_nsenter && chmod +x ./nsenter && sudo mv ./nsenter /usr/local/bin/ && which nsenter &&\
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.4/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/ &&\
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ &&\
 curl -L https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sudo bash &&\
-sudo minikube start --vm-driver=none --kubernetes-version=v1.9.4 &&\
+sudo -E minikube start --vm-driver=none &&\
 minikube update-context &&\
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done &&\
 while ! ( helm init --history-max 1 --upgrade --wait &&\
